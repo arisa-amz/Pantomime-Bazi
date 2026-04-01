5636c7b15a84c4f43a7bf218a7cb5a1a461c0fe1
Erfan <erfanyarahmadi96@gmail.com>
2026-04-01 10:00:25 +0200

    Initial Commit

---
+++ Pantomime Bazi/Pantomime_BaziApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pantomime_BaziApp.swift
+//  Pantomime Bazi
+//
+//  Created by Erfan Yarahmadi on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Pantomime_BaziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
